393ca8fcccb87d3c867a1bc355fedcce3d54d162
Arun Raghunath <arun2aruns@gmail.com>
2026-03-06 10:48:33 +0000

    feat: Support custom static routes

--- jamanthi/app.py
+++ jamanthi/app.py
@@ -1,29 +1,73 @@
 from wsgiref.types import StartResponse
 from typing import Any
+from collections.abc import Callable
+
 import logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
+ViewFunc = Callable[..., str|bytes]
+
 # A dict of appropriate HTTP status codes
 HTTP_STATUS_MAPPINGS: dict[int, str] = {
-    200: '200 OK'
+    200: '200 OK',
+    404: '404 NOT FOUND',
 }
+
+class NotFound(Exception):
+    status = 404
+
+def not_found(start_response: StartResponse) -> list[bytes]:
+    start_response(HTTP_STATUS_MAPPINGS[404], [('Content-Type', 'text/plain')])
+    return [b"Page Not Found"]
+
 class Jamanthi:
     def __init__(self, name: str) -> None:
         self.name = name
+        # Records the handlers for a method and path combination
+        self.routes: dict[str, dict[str, ViewFunc]] = {
+            'GET': {},
+        }
+        logger.info(f"[Jamanthi] App initialized with name {name}")
 
     # Callable which web servers will call invoke when a request arrives
     def __call__(self, environ: dict[str, Any], start_response: StartResponse) -> list[bytes]:
 
         status = 200
         content_type = 'text/html'
+
+        try:
+            handler = self.find_handler(environ)
+        except NotFound:
+            logger.error(f"[Jamanthi] Handler not registered")
+            return not_found(start_response)
+
+        body = handler()
+        if isinstance(body, str):
+            body = body.encode()
         headers = [('Content-type', content_type)]
         status_text = HTTP_STATUS_MAPPINGS[status]
             
         start_response(status_text, headers)
-        return [b"Welcome to Jamanthi Framework!!"]
+        return [body]
         
+    def get(self, path: str) -> Callable[[ViewFunc], ViewFunc]:
+        logger.info(f"[Jamanthi] Handler registered for 'GET' and  '{path}'")
+        def wrapper(handler: ViewFunc) -> ViewFunc:
+            self.routes['GET'][path] = handler
+            return handler
+        return wrapper
 
+    def find_handler(self, environ: dict[str, Any]) -> ViewFunc:
+        method = environ.get('REQUEST_METHOD', 'GET')
+        path = environ.get('PATH_INFO', '/')
+        handler = self.routes[method].get(path, None)
+        if handler is None:
+            logger.error(f"[Jamanthi] Path '{path}' not found for '{method}'")
+            raise NotFound(f"[Jamanthi] Path '{path}' not found for '{method}'")
+        logger.info(f"[Jamanthi] Handler successfully retrieved for '{method}' and  '{path}'")
+        return handler
+    
     def run(self, host: str = "0.0.0.0", port: int = 8000)->None:
         from wsgiref.simple_server import make_server
 
@@ -33,6 +77,6 @@ class Jamanthi:
         
         try:
             srv.serve_forever()
-        except KeyboardInterrupt:
+        except KeyboardInterrupt: # Gracefully shutdown using Ctrl+C
             print(f"[Jamanthi] Shuttinf Down!!")
             srv.shutdown()
--- portfolio/main.py
+++ portfolio/main.py
@@ -2,4 +2,8 @@ from jamanthi import Jamanthi
 
 app = Jamanthi(__name__)
 
+@app.get("/")
+def home()->str:
+    return f"Welcome to my Portfolio Website"
+    
 app.run()
